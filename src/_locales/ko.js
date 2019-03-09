591e898b01f94382d8be5141deb1eed5349cd9fc
taeuk.kang <gmdkrl@hanyang.ac.kr>
2019-03-10 02:04:05 +0900

    [+기능] 다국어 처리 기능 구현

---
+++ src/_locales/ko.js
@@ -0,0 +1,4 @@
+export default {
+	APP_NAME: `한양 챗봇`,
+	APP_DESCRIPTION: `시험용 한양챗봇`,
+}
---
+++ src/components/common/i18n-contents.js
@@ -0,0 +1,19 @@
+import locale_ko from '../../_locales/ko.js'
+
+i18next.init({
+	lng: `ko`,
+	debug: true,
+	resources: {
+		ko: {
+			translation: locale_ko,
+		},
+	},
+}).then(() => {
+	const content = document.querySelectorAll(`[i18n-content]`)
+	if (content) {
+		content.forEach(node => {
+			const key = node.getAttribute(`i18n-content`)
+			node.innerHTML = i18next.t(key)
+		})
+	}
+})
