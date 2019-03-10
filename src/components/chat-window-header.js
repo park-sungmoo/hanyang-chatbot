da845b992894a731e505b70c4c26b22158fb7ab7
taeuk.kang <gmdkrl@hanyang.ac.kr>
2019-03-11 02:46:29 +0900

    [+기능] 채팅창 상단부 UI 전부 삽입

--- src/components/chat-window-header.js
+++ src/components/chat-window-header.js
@@ -11,11 +11,15 @@ class ChatWindowHeader extends HTMLElement {
 	connectedCallback() {
 		this.shadowRoot.querySelector(`.submenu-picture`).addEventListener(`click`, this.onClickSubmenuPicture, true)
 		this.shadowRoot.querySelector(`.submenu-search`).addEventListener(`click`, this.onClickSubmenuSearch, true)
+		this.shadowRoot.querySelector(`.menu-button`).addEventListener(`click`, this.onClickMenu, true)
+		this.shadowRoot.querySelector(`.menu-alarm`).addEventListener(`click`, this.onClickAlarm.bind(this), true)
 	}
 
 	disconnectedCallback() {
 		this.shadowRoot.querySelector(`.submenu-picture`).removeEventListener(`click`, this.onClickSubmenuPicture, true)
 		this.shadowRoot.querySelector(`.submenu-search`).removeEventListener(`click`, this.onClickSubmenuSearch, true)
+		this.shadowRoot.querySelector(`.menu-button`).removeEventListener(`click`, this.onClickMenu, true)
+		this.shadowRoot.querySelector(`.menu-alarm`).removeEventListener(`click`, this.onClickAlarm, true)
 	}
 
 	onClickSubmenuPicture() {
@@ -24,6 +28,29 @@ class ChatWindowHeader extends HTMLElement {
 
 	onClickSubmenuSearch() {
 		alert(i18next.t(`NO_IMPLEMENT`))
+	}
+
+	onClickMenu() {
+		alert(i18next.t(`NO_IMPLEMENT`))
+	}
+
+	onClickAlarm() {
+		this.toggleAlarmIcon()
+	}
+
+	toggleAlarmIcon() {
+		const alarmButton = this.shadowRoot.querySelector(`.menu-alarm`)
+		if(alarmButton.classList.contains(`on`)) {
+			alarmButton.classList.remove(`on`)
+			alarmButton.innerHTML = `
+			<path fill="#65717C" d="M633.82 458.1l-90.62-70.05c.19-1.38.8-2.66.8-4.06.05-7.55-2.61-15.27-8.61-21.71-19.32-20.76-55.47-51.99-55.47-154.29 0-77.7-54.48-139.9-127.94-155.16V32c0-17.67-14.32-32-31.98-32s-31.98 14.33-31.98 32v20.84c-40.33 8.38-74.66 31.07-97.59 62.57L45.47 3.37C38.49-2.05 28.43-.8 23.01 6.18L3.37 31.45C-2.05 38.42-.8 48.47 6.18 53.9l588.35 454.73c6.98 5.43 17.03 4.17 22.46-2.81l19.64-25.27c5.42-6.97 4.17-17.02-2.81-22.45zM157.23 251.54c-8.61 67.96-36.41 93.33-52.62 110.75-6 6.45-8.66 14.16-8.61 21.71.11 16.4 12.98 32 32.1 32h241.92L157.23 251.54zM320 512c35.32 0 63.97-28.65 63.97-64H256.03c0 35.35 28.65 64 63.97 64z"></path>
+			`
+			return
+		}
+		alarmButton.classList.add(`on`)
+		alarmButton.innerHTML = `
+		<path fill="currentColor" d="M224 512c35.32 0 63.97-28.65 63.97-64H160.03c0 35.35 28.65 64 63.97 64zm215.39-149.71c-19.32-20.76-55.47-51.99-55.47-154.29 0-77.7-54.48-139.9-127.94-155.16V32c0-17.67-14.32-32-31.98-32s-31.98 14.33-31.98 32v20.84C118.56 68.1 64.08 130.3 64.08 208c0 102.3-36.15 133.53-55.47 154.29-6 6.45-8.66 14.16-8.61 21.71.11 16.4 12.98 32 32.1 32h383.8c19.12 0 32-15.6 32.1-32 .05-7.55-2.61-15.27-8.61-21.71z"></path>
+		`
 	}
 
 	render() {
@@ -36,12 +63,13 @@ class ChatWindowHeader extends HTMLElement {
 				<div class='name-submenu'>
 					<div class='name'>${i18next.t(`LISTENER_NAME`)}</div>
 					<div class='submenu'>
-						<img class='submenu-picture' src='/hanyang-chatbot/src/images/submenu-picture.svg' height='16' width='16' title='모아보기'>
-						<img class='submenu-search' src='/hanyang-chatbot/src/images/submenu-search.svg' height='14' width='14' title='검색'>
+						<svg class='submenu-picture' height='16' width='16' aria-hidden="true" focusable="false" data-prefix="fas" data-icon="image" class="svg-inline--fa fa-image fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M464 448H48c-26.51 0-48-21.49-48-48V112c0-26.51 21.49-48 48-48h416c26.51 0 48 21.49 48 48v288c0 26.51-21.49 48-48 48zM112 120c-30.928 0-56 25.072-56 56s25.072 56 56 56 56-25.072 56-56-25.072-56-56-56zM64 384h384V272l-87.515-87.515c-4.686-4.686-12.284-4.686-16.971 0L208 320l-55.515-55.515c-4.686-4.686-12.284-4.686-16.971 0L64 336v48z"></path></svg>
+						<svg class='submenu-search' height='14' width='14' aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" class="svg-inline--fa fa-search fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg>
 					</div>
 				</div>
-				<div class='menu'>
-					
+				<div class='menu'>		
+					<svg class='menu-alarm on' height='20' width='32' aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bell" class="svg-inline--fa fa-bell fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224 512c35.32 0 63.97-28.65 63.97-64H160.03c0 35.35 28.65 64 63.97 64zm215.39-149.71c-19.32-20.76-55.47-51.99-55.47-154.29 0-77.7-54.48-139.9-127.94-155.16V32c0-17.67-14.32-32-31.98-32s-31.98 14.33-31.98 32v20.84C118.56 68.1 64.08 130.3 64.08 208c0 102.3-36.15 133.53-55.47 154.29-6 6.45-8.66 14.16-8.61 21.71.11 16.4 12.98 32 32.1 32h383.8c19.12 0 32-15.6 32.1-32 .05-7.55-2.61-15.27-8.61-21.71z"></path></svg>
+					<svg class='menu-button' height='20' width='32' aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" class="svg-inline--fa fa-bars fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path></svg>
 				</div>
 			</main>
 		`
@@ -50,6 +78,10 @@ class ChatWindowHeader extends HTMLElement {
 
 const style = html`
 <style scoped>
+	* {
+		user-select: none;
+	}
+
 	main {
 		width: 100%;
 		height: 100%;
@@ -87,16 +119,32 @@ const style = html`
 		color: #4A4C4E;
 	}
 
-	.submenu > img {
+	.submenu > svg {
 		vertical-align: middle;	
-		opacity: 0.4;
-		padding-left: 2px;		
-		color: #4A4C4E;
+		padding-left: 2px;
 		cursor: pointer;
+		color: #65717C;
 	}
 
-	.submenu > img:hover {
-		opacity: 1;		
+	.submenu > svg:hover, .menu > svg:hover {
+		color: black;
+	}
+
+	.menu {
+		text-align: center;
+	}
+
+	.menu > svg {
+		vertical-align: middle;	
+		cursor: pointer;		
+		position:relative;
+		top: 50%;
+		transform: translateY(-50%);
+		color: #65717C;
+	}
+
+	.menu-alarm:not(.on) {
+		left: -3px;
 	}
 </style>
 `
