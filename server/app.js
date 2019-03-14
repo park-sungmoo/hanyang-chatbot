580d65407e189036a255cd0307d7b0d8c61604a3
taeuk.kang <gmdkrl@hanyang.ac.kr>
2019-03-15 07:35:10 +0900

    [-/+수정] server 인덱스 페이지, html > pug 재변경, error 스택 관리를 위해서...

--- server/app.js
+++ server/app.js
@@ -11,8 +11,8 @@ const app = express()
 
 // view engine setup
 app.set(`views`, path.join(__dirname, `views`))
-app.engine(`html`, require(`ejs`).renderFile)
-app.set(`view engine`, `html`)
+// app.engine(`html`, require(`ejs`).renderFile)
+app.set(`view engine`, `pug`)
 
 app.use(logger(`dev`))
 app.use(express.json())
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,5 @@
-/* eslint-disable no-var */
-// outside strict 모드에서 아직 const, let 사용불가로 var 사용
-var express = require(`express`)
-var router = express.Router()
+const express = require(`express`)
+const router = express.Router()
 
 /* GET home page. */
 router.get(`/`, (req, res, next) => {
